6aa6763314f0ff863e1ce992d9f411781069d4a3
naddda0813 <jiyeah00813@gmail.com>
2022-05-30 11:56:33 +0900

    commit

--- lib/pages/insert.dart
+++ lib/pages/insert.dart
@@ -23,27 +23,114 @@ class _InsertState extends State<Insert> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text(date),
-            TextField(
-              controller: volume,
-              decoration: const InputDecoration(labelText: '물의 용량을 입력하세요.'),
-              keyboardType: TextInputType.number,
+            const Text("Today",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w600
             ),
-            ElevatedButton(
+            ),
+            Text(date,
+             style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w200
+            ),
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    //
+                  }
+                , child: const Text("물")
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    //Navigator.of(context).pop();
+                  }
+                , child: const Text("커피")
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    //Navigator.of(context).pop();
+                  }
+                , child: const Text("탄산음료")
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                  //Navigator.of(context).pop();
+                  }
+                , child: const Text("차")
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    //Navigator.of(context).pop();
+                  }
+                , child: const Text("주스")
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    //
+                  }
+                , child: const Text("100")
+                ),
+                TextButton(
+                  onPressed: () {
+                    //
+                  }
+                , child: const Text("200")
+                ),
+                TextButton(
+                  onPressed: () {
+                    //
+                  }
+                , child: const Text("300")
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                TextButton(onPressed: () {
+                  // showdialog로 용량 바꾸기 
+                  //volumDialog();
+                }, child: Text("컵 바꾸기"),
+                style : TextButton.styleFrom(
+                  primary: Colors.black87
+                ),
+                ),
+              ],
+            )
+            // TextField(
+            //   controller: volume,
+            //   decoration: const InputDecoration(labelText: '물의 용량을 입력하세요.'),
+            //   keyboardType: TextInputType.number,
+            // ),
+            ,ElevatedButton(
                 onPressed: () {
                   insertWater();
                 },
-                child: const Text('물주기'))
+                child: const Text('더하기'))
           ],
         ),
       ),
     );
   }
-
+  //function
   insertWater() async {
-    String water_kind = '물';
+    //현재 물로 세팅됨 
+    //String water_kind = '물';
+    //test
+     String water_kind = '커피';
     var url = Uri.parse(
         'http://localhost:8080/Flutter/water_drinking/insert.jsp?id=${Static.id}&date=${Static.date}&volume=${volume.text}&kind=$water_kind');
     var response = await http.get(url);
